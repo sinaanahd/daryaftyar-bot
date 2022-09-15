b90f81b80030e19aab21d0a2c4a6130731056772
SiNAAnahid <sina.anahid@outlook.com>
2022-09-15 16:45:26 +0430

    main-apge html created

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+// variables
+
+const footer_btn_checkout = document.querySelector('.footer-menu.it-1');
+const footer_btn_home = document.querySelector('.footer-menu.it-2');
+const footer_btn_cart = document.querySelector('.footer-menu.it-3');
+
+// classes
+
+// events
+
+// functions
+
+// etc
