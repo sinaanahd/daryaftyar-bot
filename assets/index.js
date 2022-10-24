e593090edb8fec1aff638a7b375a2bb61fd04006
SiNAAnahid <sina.anahid@outlook.com>
2022-10-24 20:33:07 +0330

    new ui : static contents , welcome-page and main-page created and id restored

--- assets/index.js
+++ assets/index.js
@@ -255,10 +255,10 @@ let let_the_cart = false;
 // ! events
 // * ids with telegram object
 // filling the user via telegram object
-//const us_id = window.Telegram.WebApp.initData;
+const us_id = window.Telegram.WebApp.initData;
 // spiliting data to find the id of the user
-//const final_id = us_id.split("%22")[2].split("3A")[1].split("%")[0];
-const final_id = "341393410";
+const final_id = us_id.split("%22")[2].split("3A")[1].split("%")[0];
+//const final_id = "341393410";
 
 // ! loading complete method
 //documnet load to render first page
